b82a65d09d43b4310aa720d06799fb1a2995315c
Ian Hyde <mrhyde801@Ians-MacBook-Pro.local>
2022-04-23 20:19:34 -0600

    pushing firebase authentication

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,9 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDCQop5XQ8MWb0ZyWVueRTPQPARlb7LpJs",
+    authDomain: "mymoviesearchapp.firebaseapp.com",
+    projectId: "mymoviesearchapp",
+    storageBucket: "mymoviesearchapp.appspot.com",
+    messagingSenderId: "750362087376",
+    appId: "1:750362087376:web:89df24e00b51b2f2b9f3d5",
+    measurementId: "G-LP94NR99YK"
+  }
 };
-
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
